b85d14f94a0dd8ec6b1645d2c43f45104348f9f3
PLN141 <pedrolaurindoneto>
2024-09-11 20:56:32 -0300

    subindo a api

---
+++ todos.js
@@ -0,0 +1,5 @@
+// 1- consumindo uma api com o fetch
+fetch('https://api.github.com/users/pln141')
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error(error))
